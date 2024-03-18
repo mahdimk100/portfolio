d34f1eb94c598f31cbd630e3a25785b4149cf7fc
mahdi khodaei <109039802+mahdimk100@users.noreply.github.com>
2024-03-18 18:44:13 +0330

    fix serializers

--- portfoilo/serializers.py
+++ portfoilo/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['id', 'username', 'email', 'groups']
