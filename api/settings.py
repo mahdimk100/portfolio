e565e126d1a9dd2567f1a155fe31446f244a3013
mahdi khodaei <109039802+mahdimk100@users.noreply.github.com>
2024-03-18 18:33:59 +0330

    fix complate

--- api/settings.py
+++ api/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'portfoilo.apps.PortfoiloCobfig',
+    'rest_framework',
+    'portfoilo.apps.PortfoiloConfig',
 ]
 
 MIDDLEWARE = [
