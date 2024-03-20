5a5b0804623436c7548092ed38bc2c5237606aa7
mahdi khodaei <109039802+mahdimk100@users.noreply.github.com>
2024-03-20 17:47:17 +0330

    users allow any

--- portfoilo/views.py
+++ portfoilo/views.py
@@ -11,4 +11,4 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
