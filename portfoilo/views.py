e927edbae2ce099e9528d7c54a63b0471e3ae914
mahdi khodaei <109039802+mahdimk100@users.noreply.github.com>
2024-03-20 18:57:00 +0330

    write views for Education, Work, Portfolio

--- portfoilo/views.py
+++ portfoilo/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import permissions, viewsets
-
-from .serializers import UserSerializer
+from .models import Education, Work, Portfolio
+from .serializers import UserSerializer, EducationSerializer, WorkSerializer, PortfolioSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -12,3 +12,18 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
+
+class EducationViewSet(viewsets.ModelViewSet):
+    queryset = Education.objects.all()
+    serializer_class = EducationSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class WorkViewSet(viewsets.ModelViewSet):
+    queryset = Work.objects.all()
+    serializer_class = Work
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class PortfolioViewSet(viewsets.ModelViewSet):
+    queryset = Portfolio.objects.all()
+    serializer_class = PortfolioSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
