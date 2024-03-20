ba88b096e80d54ea7a412f38704303889468484d
mahdi khodaei <109039802+mahdimk100@users.noreply.github.com>
2024-03-20 19:02:06 +0330

    write urls api app

--- portfoilo/urls.py
+++ portfoilo/urls.py
@@ -1,10 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import *
+from . import views
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', views.UserViewSet)
+router.register(r'education', views.EducationViewSet)
+router.register(r'work', views.WorkViewSet)
+router.register(r'portfolio', views.PortfolioViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
